6a7eb205c57d663875de23330cab111c40290473
yunqing <kangqing.37@gmail.com>
2020-01-10 16:37:15 +0800

    mybatis plus添加控制台打印sql日志

--- demo-mybatis-plus/src/test/java/com/yunqing/demomybatisplus/DemoMybatisPlusApplicationTests.java
+++ demo-mybatis-plus/src/test/java/com/yunqing/demomybatisplus/DemoMybatisPlusApplicationTests.java
@@ -3,6 +3,7 @@ package com.yunqing.demomybatisplus;
 import cn.hutool.core.lang.Snowflake;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.yunqing.demomybatisplus.pojo.Role;
 import com.yunqing.demomybatisplus.pojo.User;
@@ -24,7 +25,8 @@ class DemoMybatisPlusApplicationTests {
     private Snowflake snowflake;
 
     /**
-     * ActiveRecord添加
+     * ActiveRecord添加， 有选择性的添加
+     * INSERT INTO t_role ( id, role_code ) VALUES ( ?, ? )
      */
     @Test
     void insert() {
@@ -40,17 +42,23 @@ class DemoMybatisPlusApplicationTests {
 
     /**
      * ActiveRecord修改
+     * UPDATE t_role SET role_name=? WHERE id=?
+     *
+     * UPDATE t_role SET create_time=? WHERE (id = ?)
      */
     @Test
     void update() {
-        Assertions.assertTrue(new Role().setId(2L).setRoleName("管理员").updateById());
+        Assertions.assertTrue(new Role().setId(3L).setRoleName("管理员").updateById());
 
         Assertions.assertTrue(new Role().update(new UpdateWrapper<Role>().lambda()
-                .set(Role::getCreateTime, LocalDateTime.now()).eq(Role::getId, 2)));
+                .set(Role::getCreateTime, LocalDateTime.now()).eq(Role::getId, 3)));
     }
 
     /**
      * ActiveRecord查询
+     * SELECT * FROM t_user WHERE (user_status = ?)
+     *
+     * SELECT * FROM t_user
      */
     @Test
     void select() {
@@ -66,10 +74,13 @@ class DemoMybatisPlusApplicationTests {
 
     /**
      * ActiveRecord删除
+     * DELETE FROM t_role WHERE id=?
+     *
+     * DELETE FROM t_user WHERE (user_status = ? AND create_time < ? AND email = ?)
      */
     @Test
     void delete() {
-        Assertions.assertTrue(new Role().setId(2L).deleteById());
+        Assertions.assertTrue(new Role().setId(1215548293561008129L).deleteById());
 
         Assertions.assertTrue(new User().delete(new QueryWrapper<User>().lambda().eq(User::getUserStatus, "1")
         .lt(User::getCreateTime, LocalDateTime.now()).eq(User::getEmail, "yunqing_71@163.com")));
@@ -78,12 +89,19 @@ class DemoMybatisPlusApplicationTests {
     /**
      * ActiveRecord分页查询
      * *******注意配置分页插件才能生效**********
+     * SELECT COUNT(1) FROM t_user WHERE (user_status = ?)
+     *
+     * SELECT * FROM t_user WHERE (user_status = ?) LIMIT ?,?
      */
     @Test
     void selectPage() {
-        List<User> list = new User().selectPage(new Page<>(1, 1),
+        //第一页，每页1条数据
+        IPage<User> userIPage = new Page<>(1, 1);
+        List<User> list = new User().selectPage(userIPage,
                 new QueryWrapper<User>().lambda().eq(User::getUserStatus, "1"))
                 .getRecords();
+        //获取不分页总数
+        System.out.println(userIPage.getTotal());
         list.forEach(System.out::println);
     }
 
