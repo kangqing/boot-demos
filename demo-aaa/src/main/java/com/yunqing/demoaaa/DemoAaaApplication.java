a4b2dfc703a7ccdb1711c88d4ae7fc207a48e972
kangqing <kangqing.37@gmail.com>
2020-04-28 00:50:05 +0800

    demo-aaa

---
+++ demo-aaa/src/main/java/com/yunqing/demoaaa/DemoAaaApplication.java
@@ -0,0 +1,13 @@
+package com.yunqing.demoaaa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAaaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoAaaApplication.class, args);
+    }
+
+}
