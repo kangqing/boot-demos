600e68dd9adcd351eb24df3f1c7407883dc177d7
kangqing <kangqing.37@gmail.com>
2020-05-06 10:56:54 +0800

    aop操作日志处理

---
+++ demo-web-log/src/main/java/com/yunqing/demoweblog/controller/WebLogController.java
@@ -0,0 +1,24 @@
+package com.yunqing.demoweblog.controller;
+
+import cn.hutool.core.util.StrUtil;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author yx
+ * @date 2020/5/6 10:12
+ */
+@RestController
+@RequestMapping("/aoplog")
+public class WebLogController {
+
+    @GetMapping("/book")
+    @ApiOperation("根据id查询书名")
+    public String log(@RequestParam String id) {
+        return StrUtil.format("id为{}的书是{}", id, "mysql必知必会");
+    }
+
+}
