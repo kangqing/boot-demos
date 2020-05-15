64bf73c6fce14201b151cd78487effaaff94cf60
kangqing <kangqing.37@gmail.com>
2020-05-15 10:48:51 +0800

    http client的使用

---
+++ demo-json-result/src/main/java/com/yunqing/demojsonresult/User.java
@@ -0,0 +1,16 @@
+package com.yunqing.demojsonresult;
+
+import lombok.Data;
+
+/**
+ * @author yx
+ * @description
+ * @date 2020/5/15 9:59
+ */
+@Data
+public class User {
+
+    private String username;
+
+    private String password;
+}
--- demo-json-result/src/main/java/com/yunqing/demojsonresult/controller/TestController.java
+++ demo-json-result/src/main/java/com/yunqing/demojsonresult/controller/TestController.java
@@ -1,11 +1,10 @@
 package com.yunqing.demojsonresult.controller;
 
+import cn.hutool.core.util.StrUtil;
+import com.yunqing.demojsonresult.User;
 import com.yunqing.demojsonresult.utils.JsonResult;
-import com.yunqing.demojsonresult.utils.ResultCode;
 import com.yunqing.demojsonresult.utils.ResultTool;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author yx
@@ -15,12 +14,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/test")
 public class TestController {
 
-    @GetMapping("/result")
-    public JsonResult test() throws Exception {
+    @GetMapping("/query")
+    public JsonResult test() {
         String aa = "Hello, JsonResult";
-        if (!aa.contains("1")) {
-            throw new Exception("nnnnnn");
-        }
         return ResultTool.success(aa);
+    }
+
+    @PostMapping("/add")
+    public JsonResult test1(@RequestParam String aa) {
+        return ResultTool.success(aa);
+    }
+
+    @PostMapping("/update")
+    public JsonResult test2(@RequestBody User user) {
+        return ResultTool.success(StrUtil.format("接收到的参数中用户名是{}", user.getUsername()));
+    }
+
+    @GetMapping("/delete")
+    public JsonResult test3(@RequestParam String id) {
+        return ResultTool.success(StrUtil.format("id为{}的列不存在", id));
     }
 }
