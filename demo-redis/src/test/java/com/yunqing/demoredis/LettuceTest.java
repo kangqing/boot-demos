2ca4423b62d09834cb00f86c7543e4b06502e0f1
kangqing <kangqing.37@gmail.com>
2020-06-03 17:38:24 +0800

    redis Lettuce客户端连接

---
+++ demo-redis/src/test/java/com/yunqing/demoredis/LettuceTest.java
@@ -0,0 +1,88 @@
+package com.yunqing.demoredis;
+
+import io.lettuce.core.RedisClient;
+import io.lettuce.core.api.StatefulRedisConnection;
+import io.lettuce.core.api.sync.RedisCommands;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author yx
+ * @description 参考 try.redis.io
+ * @date 2020/6/3 16:08
+ */
+@SpringBootTest
+@Slf4j
+public class LettuceTest {
+
+    RedisClient redisClient;
+    StatefulRedisConnection<String, String> redisConnection;
+    RedisCommands<String, String> sync;
+
+    /**
+     * @BeforeEach 注解在非静态方法上，所有测试方法之前执行
+     * @BeforeAll 注解在静态方法上，所有测试方法之前执行
+     */
+    @BeforeEach
+    void connRedisByLettuce() {
+        redisClient = RedisClient.create("redis://127.0.0.1:6379");
+        redisConnection = redisClient.connect();
+        sync = redisConnection.sync();
+    }
+
+    /**
+     * 关闭连接
+     */
+    @AfterEach
+    void closeConn() {
+        redisConnection.close();
+        redisClient.shutdown();
+    }
+
+    /**
+     * string类型set get
+     * ttl 返回-1永不超时
+     * ttl 返回-2不存在此key
+     */
+    @Test
+    void set() {
+        log.info(sync.set("k1", "v1"));
+        sync.expire("k1", 1000); //设置1000秒超时
+        try {
+            TimeUnit.SECONDS.sleep(3);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        log.info(sync.ttl("k1").toString()); //获取剩余超时时间
+        log.info(sync.get("k1"));
+    }
+
+    /**
+     * redis 列表，相当于链表
+     * lpush 左侧添加
+     * rpush 右侧添加
+     * lrange key 0 -1 获取列表第一到最后一个
+     * lpop key 删除列表左侧第一个
+     * rpop key 删除列表右侧第一个
+     * llen key 获取当前列表长度
+     */
+    @Test
+    void redisList() {
+        sync.lpush("friends", "Tom");
+        sync.lpush("friends", "Bob");
+        sync.rpush("friends", "peter");
+        log.info(sync.llen("friends").toString());
+        List<String> friends = sync.lrange("friends", 0, -1);
+        friends.forEach(System.out::println);
+        sync.lpop("friends");
+        sync.rpop("friends");
+        List<String> friends2 = sync.lrange("friends", 0, -1);
+        friends2.forEach(System.out::println);
+    }
+}
