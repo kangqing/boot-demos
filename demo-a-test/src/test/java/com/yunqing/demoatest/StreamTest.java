b5f58b383a90a3a78a92f7271bbcfcfbfb5d566c
kangqing <kangqing.37@gmail.com>
2020-07-02 22:10:07 +0800

    stream()流操作1

--- demo-a-test/src/test/java/com/yunqing/demoatest/StreamTest.java
+++ demo-a-test/src/test/java/com/yunqing/demoatest/StreamTest.java
@@ -1,5 +1,6 @@
 package com.yunqing.demoatest;
 
+import cn.hutool.core.util.ReUtil;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
@@ -9,9 +10,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.BeanUtils;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 /**
  * @author yx
@@ -23,20 +24,35 @@ import java.util.stream.Collectors;
 public class StreamTest {
 
     private List<Student> list;
+    private List<List<Student>> listFlat;
 
     @BeforeEach//注解在非静态方法上
     void init() {
         list = Arrays.asList(
-                new Student(1, "盖伦1", 20, 66.0),
-                new Student(2, "赵信", 21, 80.5),
-                new Student(3, "乐芙兰1", 202, 90.0),
+                new Student(1, "盖伦1", 200, 66.0),
+                new Student(2, "赵信", 21, 90.0),
+                new Student(3, "乐芙兰1", 21, 90.0),
                 new Student(4, "李青", 50, 100d),
                 new Student(5, "泰达米尔", 600, 90d)
+        );
+
+        listFlat = Arrays.asList(
+                Arrays.asList(
+                        new Student(1, "盖伦1", 200, 66.0),
+                        new Student(2, "赵信", 21, 90.0)
+                ),
+                Arrays.asList(
+                        new Student(3, "乐芙兰1", 21, 90.0),
+                        new Student(4, "李青", 50, 100d)
+                ),
+                Collections.singletonList(
+                        new Student(5, "泰达米尔", 600, 90d)
+                )
         );
     }
 
     /**
-     * Filter
+     * filter
      * @DESC 过滤出list中学生名字中包括 "1" 的学生集合，
      *       并使用peek()利用其返回Stream<Student>直接以流的方式实现打印过滤出的学生
      *
@@ -60,7 +76,7 @@ public class StreamTest {
     }
 
     /**
-     * Map
+     * map
      * @DESC 可以将List里面的对象转化成新的对象
      * 1.将学生集合里不小于90分的作为精英学生过滤出来,并打印
      * 2.获取所有学生的 name 集合,并打印
@@ -80,6 +96,87 @@ public class StreamTest {
                 .peek(System.out::println)
                 .collect(Collectors.toList());
         Assertions.assertEquals(5, nameList.size());
+
+    }
+
+    /**
+     * flatMap
+     * @DESC 将嵌套列表转换为普通列表，例如 List<List<Student>> 转化为 List<Student>
+     * 1.将嵌套列表转化成普通列表，断言转化结果 = list
+     * 2.转化后获取列表的 id 集合并打印
+     * 3.转化后再提取集合的分数，求平均值，打印平均值
+     */
+    @Test
+    void flatMapTest() {
+        List<Student> collect = listFlat.stream().flatMap(Collection::stream)
+                .peek(System.out::println)
+                .collect(Collectors.toList());
+        Assertions.assertEquals(collect, list);
+
+        List<Integer> idList = listFlat.stream().flatMap(Collection::stream)
+                .peek(System.out::println)
+                .flatMapToInt(student -> IntStream.of(student.getId()))
+                .peek(System.out::println)
+                .boxed()
+                .collect(Collectors.toList());
+        Assertions.assertEquals(idList.size(), 5);
+
+        listFlat.stream().flatMap(Collection::stream)
+                .mapToDouble(Student::getScore)
+                .average()
+                .ifPresent(System.out::println);
+
+    }
+
+    /**
+     * sorted
+     * @DESC 排序
+     * 1.先按照分数倒叙排列，如果分数相同按照年龄正序排列，如果年龄相同按照 id 正序排列，打印
+     * 2.如果 reversed() 写在最后，则全部按照倒叙排列
+     */
+    @Test
+    void sortedTest() {
+        List<Student> collect = list.stream().sorted(Comparator.comparing(Student::getScore).reversed()
+                .thenComparing(Student::getAge)
+                .thenComparing(Student::getId))
+                .peek(System.out::println)
+                .collect(Collectors.toList());
+        Assertions.assertEquals("李青", collect.get(0).getName());
+
+        log.info("-------------------------------------------------------------");
+
+        List<Student> collect2 = list.stream().sorted(Comparator.comparing(Student::getScore)
+                .thenComparing(Student::getAge)
+                .thenComparing(Student::getId).reversed())
+                .peek(System.out::println)
+                .collect(Collectors.toList());
+        Assertions.assertEquals("李青", collect2.get(0).getName());
+    }
+
+    /**
+     * match
+     * @DESC 验证list中的每一项是否匹配我们的条件
+     * allMatch 全都匹配
+     * anyMatch 任意匹配
+     * noneMatch 全不匹配
+     *
+     * 设置匹配条件为正则表达式 ^[1-9]\\d*$ 正整数
+     * 1.验证list的id是否全都匹配条件
+     * 2.临时改变list的第一个年龄为 -1 验证是否年龄任意一个匹配条件
+     * 3.验证分数是否全不匹配条件
+     */
+    @Test
+    void matchTest() {
+        boolean b = list.stream().allMatch(item -> ReUtil.isMatch("^[1-9]\\d*$", item.getId().toString()));
+        log.info("输出list的id是否全部匹配条件,结果：----------[{}]", b);
+
+        list.get(0).setAge(-1);
+        list.forEach(System.out::println);
+        boolean b1 = list.stream().anyMatch(item -> ReUtil.isMatch("^[1-9]\\d*$", item.getAge().toString()));
+        log.info("输出list的年龄是否任意匹配条件,结果：----------[{}]", b1);
+
+        boolean b2 = list.stream().noneMatch(item -> ReUtil.isMatch("^[1-9]\\d*$", item.getScore().toString()));
+        log.info("输出list的年龄是否都不匹配条件,结果：----------[{}]", b2);
 
     }
 
