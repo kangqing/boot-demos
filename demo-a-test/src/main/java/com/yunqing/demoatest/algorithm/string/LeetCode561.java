99effc19c86bb3fa1c1b548e93e8ec042cb2b762
kangqing <kangqing.37@gmail.com>
2020-08-08 08:18:40 +0800

    LeetCode561 数组拆分 I

--- demo-a-test/src/main/java/com/yunqing/demoatest/algorithm/string/LeetCode561.java
+++ demo-a-test/src/main/java/com/yunqing/demoatest/algorithm/string/LeetCode561.java
@@ -9,19 +9,26 @@ import java.util.*;
  */
 public class LeetCode561 {
     public static void main(String[] args) {
-
+        Solution561 solution561 = new Solution561();
+        int[] nums = new int[]{10, 3, -1, 0, 2, 9, 11, -2};
+        System.out.println("结果是：" + solution561.arrayPairSum(nums));
     }
 }
 
 class Solution561 {
     public int arrayPairSum(int[] nums) {
-        List<Integer> list = Arrays.asList(nums);
+        List<Integer> list = new ArrayList<>();
+        for (int num : nums) {
+            list.add(num);
+        }
         Collections.sort(list);
+        list.forEach(System.out::println);
         int sum = 0;
         for (int i = 0; i < list.size(); i++) {
             if (i % 2 == 0) {
                 sum += list.get(i);
             }
         }
+        return sum;
     }
 }
