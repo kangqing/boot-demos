29ea6b9a455be59d2c1ac5663a641b2749266502
ls-k <lushuang_kang@163.com>
2020-08-10 07:15:57 +0800

    LeetCode209 长度最小的连续子数组 LeetCode485 最大连续1的个数

---
+++ demo-a-test/src/main/java/com/yunqing/demoatest/algorithm/array/LeetCode209.java
@@ -0,0 +1,56 @@
+package com.yunqing.demoatest.algorithm.array;
+
+/**
+ * LeetCode209 长度最小的连续子数组
+ * @author yunqing
+ * @since 2020/8/10 6:41
+ */
+public class LeetCode209 {
+    public static void main(String[] args) {
+        Solution209 solution209 = new Solution209();
+        int[] arr = new int[]{2,3,1,2,4,3};
+        System.out.println(solution209.minSubArrayLen_1(7, arr));
+    }
+}
+
+class Solution209 {
+    /**
+     * TODO 参考答案
+     * 双指针解法，时间复杂度O(n)
+     * @param s
+     * @param nums
+     * @return
+     */
+    public int minSubArrayLen_1(int s, int[] nums) {
+        int len = nums.length;
+        if (len == 0) {
+            return 0;
+        }
+        int fast = 0;
+        int slow = 0;
+        int sum = 0;
+        int min = Integer.MAX_VALUE;
+        while(fast < len) {
+            sum += nums[fast];
+            while (sum >= s) {
+                min = Math.min(min, fast - slow + 1);
+                sum -= nums[slow];
+                slow++;
+            }
+            fast++;
+        }
+        return min == Integer.MAX_VALUE ? 0 : min;
+    }
+
+    /**
+     * TODO 还没完成二分查找法
+     * 二分查找法，时间复杂度O(log n)
+     * @param s
+     * @param nums
+     * @return
+     */
+    public int minSubArrayLen_2(int s, int[] nums) {
+
+        return 0;
+    }
+}
--- demo-a-test/src/main/java/com/yunqing/demoatest/algorithm/array/LeetCode485.java
+++ demo-a-test/src/main/java/com/yunqing/demoatest/algorithm/array/LeetCode485.java
@@ -9,21 +9,25 @@ package com.yunqing.demoatest.algorithm.array;
 public class LeetCode485 {
     public static void main(String[] args) {
         Solution485 solution485 = new Solution485();
-        int[] arr = new int[]{1,0,1,1,0,1};
+        int[] arr = new int[]{1};
         System.out.println(solution485.findMaxConsecutiveOnes(arr));
     }
 }
 
+/**
+ * 双指针
+ */
 class Solution485 {
     public int findMaxConsecutiveOnes(int[] nums) {
         int max = 0;
-        int t = 0;
+        int count = 0;
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] == 1) {
-                t++;
-            } else {
-                max = Math.max(t, max);
-                t = 0;
+                count++;
+            }
+            if (nums[i] == 0 || i == nums.length -1) {
+                max = Math.max(count, max);
+                count = 0;
             }
         }
         return max;
