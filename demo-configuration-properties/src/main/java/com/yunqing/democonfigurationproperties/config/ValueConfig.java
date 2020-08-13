fa895d08d102ad586f7d048c4bd885be76b98159
zl_yunqing163 <yunqing_71@163.com>
2020-08-13 10:58:17 +0800

    springboot中使用@Value加载配置文件数据

---
+++ demo-configuration-properties/src/main/java/com/yunqing/democonfigurationproperties/config/ValueConfig.java
@@ -0,0 +1,36 @@
+package com.yunqing.democonfigurationproperties.config;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.io.support.DefaultPropertySourceFactory;
+
+/**
+ * @author yx
+ * @description
+ * @date 2020/8/13 10:21
+ */
+@Configuration
+@PropertySource(value = {"classpath:valueconfig.yml"}, factory = PropertySourceFactory.class)
+public class ValueConfig {
+    @Value("${yunqing.name}")
+    private String name;
+
+    private static String email;
+
+    @Value("${yunqing.email}")
+    public void setEmail(String email) {
+        ValueConfig.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
