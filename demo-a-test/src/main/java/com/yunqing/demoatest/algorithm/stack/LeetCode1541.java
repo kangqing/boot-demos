96a5d3c6c30e890b79c26b5cedb6708aa8e8d0ec
kangqing <kangqing.37@gmail.com>
2020-08-15 00:13:36 +0800

    LeetCode1541 平衡括号字符串的最少插入次数

--- demo-a-test/src/main/java/com/yunqing/demoatest/algorithm/stack/LeetCode1541.java
+++ demo-a-test/src/main/java/com/yunqing/demoatest/algorithm/stack/LeetCode1541.java
@@ -1,7 +1,5 @@
 package com.yunqing.demoatest.algorithm.stack;
 
-import java.util.ArrayDeque;
-import java.util.Iterator;
 
 /**
  * LeetCode1541 平衡括号字符串的最少插入次数
@@ -11,11 +9,10 @@ import java.util.Iterator;
 public class LeetCode1541 {
     public static void main(String[] args) {
         Solution1541 solution1541 = new Solution1541();
-        var s = "(()))";
+        var s = ")))))))";
         System.out.println(solution1541.minInsertions(s));
     }
 }
-0000000000000000000000000000000000000000000000000000000000000000000000000
 
 /**
  * 因为一个( 需要匹配 ))
@@ -24,61 +21,30 @@ public class LeetCode1541 {
  */
 class Solution1541 {
     public int minInsertions(String s) {
-        int count = 0;
-        var stack = new ArrayDeque<Character>();
-        char[] chars = s.toCharArray();
-        for (char c : chars) {
-            if (c == '(') {
-                stack.push(c);
+        int ans = 0; //补全次数
+        int left = 0; //左括号次数
+        int len = s.length();
+        for (int i = 0; i < len; i++) {
+            if (s.charAt(i) == '(') {
+                left++;
             } else {
-                if (stack.isEmpty()) {
-                    stack.push(']');
-                } else if (stack.peek() == ']'){
-                    stack.pop();
-                    stack.push(c);
-                } else {
-                    stack.push(']');
+                //右括号
+                if (i + 1 < len && s.charAt(i + 1) == ')') {
+                    i++;//跳到下下个
+                }else {
+                    //缺少一个右括号，加一个
+                    ans++;
+                }
+                if (left > 0) {
+                    --left;
+                }else {
+                    //缺少左括号,加一个
+                    ans++;
                 }
             }
         }
-        //创建一个新的栈，开始把对应的()删除
-        var stack1 = new ArrayDeque<Character>();
-        Iterator<Character> iterator = stack.descendingIterator();
-        while (iterator.hasNext()) {
-            var c = iterator.next();
-            if (stack1.isEmpty()) {
-                stack1.push(c);
-                continue;
-            }
-            var peek = stack1.peek();
-            if (c == '(') {
-                stack1.push(c);
-            }else if (c == ')'){
-                if (peek == '(') {
-                    stack1.pop();
-                }else{
-                    stack1.push(')');
-                }
-            } else {
-                stack1.push(')');
-                count++;
-            }
-        }
-
-        if (stack1.isEmpty()) {
-            return count;
-        }
-
-        while (!stack1.isEmpty()) {
-            var c = stack1.pop();
-            if (c == '(') {
-                count += 2;
-            } else {
-                count++;
-            }
-        }
-        return count;
-
+        ans += left * 2;
+        return ans;
 
     }
 }
