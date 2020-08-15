4990a84a99554eca4dfb57e97757376c6a836e2c
kangqing <kangqing.37@gmail.com>
2020-08-15 09:38:36 +0800

    二分查找 模板 II

---
+++ demo-a-test/src/main/java/com/yunqing/demoatest/algorithm/binarysearch/Template2.java
@@ -0,0 +1,36 @@
+package com.yunqing.demoatest.algorithm.binarysearch;
+
+/**
+ * 二分查找 模板 II
+ * @author yunqing
+ * @since 2020/8/15 6:21
+ */
+public class Template2 {
+    public static void main(String[] args) {
+        var arr = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        var target = 6;
+        var s = new SolutionTemplate2();
+        System.out.println(s.binarySearch(arr, target));
+    }
+}
+
+class SolutionTemplate2 {
+    int binarySearch(int[] nums, int target){
+        if(nums == null || nums.length == 0)
+            return -1;
+
+        int left = 0, right = nums.length;
+        while(left < right){
+            // Prevent (left + right) overflow
+            int mid = left + (right - left) / 2;
+            if(nums[mid] == target){ return mid; }
+            else if(nums[mid] < target) { left = mid + 1; }
+            else { right = mid; }
+        }
+
+        // Post-processing:
+        // End Condition: left == right
+        if(left != nums.length && nums[left] == target) return left;
+        return -1;
+    }
+}
