897a3f365b7eb7cd634a6665210e6de3408a08c5
zl_yunqing163 <yunqing_71@163.com>
2020-08-17 17:05:13 +0800

    LeetCode5488 使数组中所有元素相等的最小操作数

--- demo-a-test/src/main/java/com/yunqing/demoatest/algorithm/array/LeetCode5488.java
+++ demo-a-test/src/main/java/com/yunqing/demoatest/algorithm/array/LeetCode5488.java
@@ -12,11 +12,12 @@ public class LeetCode5488 {
     public static void main(String[] args) {
         Solution5488 solution5488 = new Solution5488();
         System.out.println(solution5488.minOperations(5));
+        System.out.println(solution5488.minOperations_1(5));
     }
 }
 
 class Solution5488 {
-    public int minOperations(int n) {
+    int minOperations(int n) {
         //第一位是1
         //最后一位是
         int first = 1;
@@ -34,5 +35,26 @@ class Solution5488 {
             count += res - integer;
         }
         return count;
+    }
+
+    /**
+     * 看了大佬思路之后
+     * @param n
+     * @return
+     */
+    int minOperations_1(int n) {
+        //先算最后的总数
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += 2 * i + 1;
+        }
+        //计算平均值
+        int avg = sum / n;
+        //计算每一个值跟平均值差的次数
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            ans += Math.abs(2 * i + 1 - avg);
+        }
+        return ans / 2;
     }
 }
