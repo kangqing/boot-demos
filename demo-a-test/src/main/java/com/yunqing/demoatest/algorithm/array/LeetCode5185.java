5c9fb45eddebff0b103015a00a96c0e79fb3f154
zl_yunqing163 <yunqing_71@163.com>
2020-08-17 17:00:21 +0800

    LeetCode5185 存在连续三个奇数的数组

--- demo-a-test/src/main/java/com/yunqing/demoatest/algorithm/array/LeetCode5185.java
+++ demo-a-test/src/main/java/com/yunqing/demoatest/algorithm/array/LeetCode5185.java
@@ -8,28 +8,15 @@ package com.yunqing.demoatest.algorithm.array;
 public class LeetCode5185 {
     public static void main(String[] args) {
         Solution5185 solution5185 = new Solution5185();
-        var arr = new int[]{1, 2, 3};
+        var arr = new int[]{1, 1, 2, 1, 3, 4, 2, 1, 3, 5};
         System.out.println(solution5185.threeConsecutiveOdds(arr));
     }
 }
 
 class Solution5185 {
-    public boolean threeConsecutiveOdds(int[] arr) {
-        int n = arr.length;
-        if (n < 3) {
-            return false;
-        }
-        for (int i = 1; i < n - 1; i++) {
-            if (arr[i] % 2 != 1) {
-                continue;
-            }
-            if (arr[i - 1] % 2 != 1) {
-                continue;
-            }
-            if (arr[i + 1] % 2 != 1) {
-                continue;
-            }
-            return true;
+    boolean threeConsecutiveOdds(int[] arr) {
+        for (int i = 0; i < arr.length -2; i++) {
+            if (arr[i] % 2 + arr[i + 1] % 2 + arr[i + 2] % 2 == 3) return true;
         }
         return false;
     }
