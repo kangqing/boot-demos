dd8d3e4ce4505e4eb81f1c9ff893551a00f36d01
zl_yunqing163 <yunqing_71@163.com>
2020-08-18 12:05:42 +0800

    LeetCode35搜索插入位置（二分法）

--- demo-a-test/src/main/java/com/yunqing/demoatest/algorithm/array/LeetCode35.java
+++ demo-a-test/src/main/java/com/yunqing/demoatest/algorithm/array/LeetCode35.java
@@ -1,20 +1,26 @@
 package com.yunqing.demoatest.algorithm.array;
 
 /**
- * @Description 搜索插入位置
+ * LeetCode35 搜索插入位置
  * @Author yunqing
- * @Data 2020/7/21 15:57
+ * @Date 2020/7/21 15:57
  */
 public class LeetCode35 {
     public static void main(String[] args) {
         int[] arr = new int[]{1,3,5,6};
         Solution35 s = new Solution35();
-        System.out.println(s.searchInsert(arr, 0));
+        int target = 7;
+        System.out.println(s.searchInsert(arr, target));
+        System.out.println(s.searchInsert_1(arr, target));
     }
 }
 
 class Solution35 {
-    public int searchInsert(int[] nums, int target) {
+
+    /**
+     * 时间复杂度 O(n)
+     */
+    int searchInsert(int[] nums, int target) {
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] < target) {
                 continue;
@@ -22,6 +28,34 @@ class Solution35 {
             return i;
         }
         return nums.length;
+    }
+
+    /**
+     * 二分法
+     * 时间复杂度 O(log n)
+     */
+    int searchInsert_1(int[] nums, int target) {
+        if (nums == null) {
+            return 0;
+        }
+        int low = 0;
+        int high = nums.length - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                low = mid + 1;
+            } else {
+                if (mid > 0 && nums[mid - 1] < target) {
+                    return mid;
+                }else {
+                    high = mid - 1;
+                }
+            }
+        }
+
+        return low;
     }
 }
 
