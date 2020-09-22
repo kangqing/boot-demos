b899efaff90a1e5eb1b1cd96cc2bbbf740f72feb
zl_yunqing163 <yunqing_71@163.com>
2020-09-22 18:01:16 +0800

    BitSet实现一个按需增长的位向量

---
+++ demo-a-test/src/main/java/com/yunqing/demoatest/readjdk/java/util/BitSetTest.java
@@ -0,0 +1,30 @@
+package com.yunqing.demoatest.readjdk.java.util;
+
+import cn.hutool.core.lang.Console;
+
+import java.util.BitSet;
+
+/**
+ * BitSet类实现一个按需增长的位向量
+ * 线程不安全
+ * 集合中所有位的初始值是false
+ * 传递一个null参数到BitSet中的任何方法都会空指针
+ * @author yx
+ * @since 2020/9/22 17:46
+ */
+public class BitSetTest {
+    public static void main(String[] args) {
+        BitSet bitSet = new BitSet();
+        BitSet bitSet1 = new BitSet(10);
+        Console.log("bitset中默认值是 {}, bitset的默认长度是 {}, 指定长度为10之后长度 {}", bitSet.get(0),
+                bitSet.length(), bitSet1.length());
+
+        bitSet.set(0);
+        bitSet.set(1);
+        bitSet.set(2);
+        Console.log("输出bitSet = {}, 长度为 {}", bitSet, bitSet.length());
+        bitSet1.set(1, 5);
+        Console.log("输出bitSet1 = {}, 长度为 {}", bitSet1, bitSet1.length());
+
+    }
+}
