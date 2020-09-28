60555a136008ac95092ddd05956f55837d78b324
zl_yunqing163 <yunqing_71@163.com>
2020-09-28 14:47:24 +0800

    构造一个二叉树类

--- demo-leetcode/src/main/java/com/yunqing/demoleetcode/algorithm/tree/TreeNode.java
+++ demo-leetcode/src/main/java/com/yunqing/demoleetcode/algorithm/tree/TreeNode.java
@@ -1,6 +1,7 @@
 package com.yunqing.demoleetcode.algorithm.tree;
 
 /**
+ * 构造一个二叉树
  * @author yx
  * @since 2020/9/17 14:40
  */
@@ -8,5 +9,17 @@ public class TreeNode {
     int val;
     TreeNode left;
     TreeNode right;
-    TreeNode(int x) { val = x; }
+
+    TreeNode() {
+    }
+
+    TreeNode(int x) {
+        val = x;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
 }
