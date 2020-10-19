e49bd97c4239dd6d7b7d7c172b3460895364e1b3
yunqing <kangqing.37@gmail.com>
2020-10-20 07:43:44 +0800

    LeetCode133 重排链表

---
+++ demo-leetcode/src/main/java/com/yunqing/demoleetcode/algorithm/linkedlist/LeetCode133.java
@@ -0,0 +1,66 @@
+package com.yunqing.demoleetcode.algorithm.linkedlist;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * LeetCode133 重排链表
+ * @author yunqing
+ * @since 2020/10/20 7:13
+ */
+public class LeetCode133 {
+    public static void main(String[] args) {
+        ListNode node = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4))));
+
+        Solution133 solution133 = new Solution133();
+        solution133.reorderList(node);
+    }
+}
+
+/**
+ * 解决方案：由于链表不支持下标访问，所以决定将其放进List中
+ */
+class Solution133 {
+    public void reorderList(ListNode head) {
+        if (head == null) return;
+        List<ListNode> list = new ArrayList<>();
+        while (head != null) {
+            list.add(head);
+            head = head.next;
+        }
+        // 双指针
+        int left = 0;
+        int right = list.size() - 1;
+        while (left < right) {
+            // L1 -> Ln
+            list.get(left).next = list.get(right);
+            left++;
+            if (left == right) {
+                break;
+            }
+            list.get(right).next = list.get(left);
+            right--;
+        }
+        list.get(left).next = null;
+    }
+}
+
+
+
+//Definition for singly-linked list.
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
