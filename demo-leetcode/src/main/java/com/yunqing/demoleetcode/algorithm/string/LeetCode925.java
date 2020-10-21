1ff65764114426acac889d5841586adb4ba89c22
zl_yunqing163 <yunqing_71@163.com>
2020-10-21 15:10:25 +0800

    LeetCode925 长按键入

---
+++ demo-leetcode/src/main/java/com/yunqing/demoleetcode/algorithm/string/LeetCode925.java
@@ -0,0 +1,31 @@
+package com.yunqing.demoleetcode.algorithm.string;
+
+/**
+ * LeetCode925 长按键入
+ * @author yx
+ * @since 2020/10/21 14:06
+ */
+public class LeetCode925 {
+    public static void main(String[] args) {
+        Solution925 solution925 = new Solution925();
+        boolean longPressedName = solution925.isLongPressedName("pyplrz", "ppyypllr");
+        System.out.println(longPressedName);
+    }
+}
+
+class Solution925 {
+    public boolean isLongPressedName(String name, String typed) {
+        int i = 0; int j = 0;
+        while (j < typed.length()) {
+            if (i < name.length() && typed.charAt(j) == name.charAt(i)) {
+                i++;
+                j++;
+            } else if (j > 0 && typed.charAt(j) == typed.charAt(j - 1)) {
+                j++;
+            } else {
+                return false;
+            }
+        }
+        return i == name.length();
+    }
+}
