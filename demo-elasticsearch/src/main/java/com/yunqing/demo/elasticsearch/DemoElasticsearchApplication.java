0d71681e2fc6806462db68939b4414ed6ba4078b
yunqing <kangqing.37@gmail.com>
2020-10-25 22:47:40 +0800

    es

---
+++ demo-elasticsearch/src/main/java/com/yunqing/demo/elasticsearch/DemoElasticsearchApplication.java
@@ -0,0 +1,13 @@
+package com.yunqing.demo.elasticsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoElasticsearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoElasticsearchApplication.class, args);
+	}
+
+}
