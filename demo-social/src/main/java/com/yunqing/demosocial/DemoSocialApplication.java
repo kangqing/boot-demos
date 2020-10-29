71ab8e07cd26c97e756f3ec037b65a0deb59e447
yunqing <kangqing.37@gmail.com>
2020-10-29 21:46:18 +0800

    建立一个demo完成社交平台登录

---
+++ demo-social/src/main/java/com/yunqing/demosocial/DemoSocialApplication.java
@@ -0,0 +1,13 @@
+package com.yunqing.demosocial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSocialApplication.class, args);
+	}
+
+}
---
+++ demo-social/src/test/java/com/yunqing/demosocial/DemoSocialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yunqing.demosocial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSocialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
