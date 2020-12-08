4d25cf91fa43e7a2d41a878e9180705b7ab6bea0
yunqing <kangqing.37@gmail.com>
2020-12-08 21:07:23 +0800

    12.8 回溯 + 剪枝

--- demo-leetcode/src/main/java/com/yunqing/demoleetcode/time/Time12m8.java
+++ demo-leetcode/src/main/java/com/yunqing/demoleetcode/time/Time12m8.java
@@ -10,13 +10,15 @@ import java.util.List;
  */
 public class Time12m8 {
     public static void main(String[] args) {
-
+        Solution12m8 s = new Solution12m8();
+        System.out.println(s.splitIntoFibonacci("123456579"));
     }
 }
 
-class Solution {
+class Solution12m8 {
     public List<Integer> splitIntoFibonacci(String S) {
         List<Integer> res = new ArrayList<>();
+        back(res, S, 0, 0, 0);
         return res;
     }
 
@@ -43,21 +45,29 @@ class Solution {
             }
             // 转化成十进制
             currLong = currLong * 10 + s.charAt(i) - '0';
+            // 如果超过int最大值,停止
             if (currLong > Integer.MAX_VALUE) {
                 break;
             }
+            // 当前截取数字转化为int
             int curr = (int) currLong;
             if (res.size() >= 2) {
+                // 当前数小于前两个数
                 if (curr < sum) {
+                    // 继续当前循环截取
                     continue;
                 } else if (curr > sum) {
+                    // 停止
                     break;
                 }
             }
+            // 等于前两个数或者res为空直接加进来
             res.add(curr);
+            // 遍历下一个字符
             if (back(res, s, i + 1, prev + curr, curr)) {
                 return true;
             } else {
+                // 删除刚才加进去的数
                 res.remove(res.size() - 1);
             }
         }
