ab84850b2c5daf59d998dc41de89e9a767f6c431
zl_yunqing163 <yunqing_71@163.com>
2020-12-21 10:20:25 +0800

    12.20 贪心 + 栈

---
+++ demo-leetcode/src/main/java/com/yunqing/demoleetcode/time/Time12m20.java
@@ -0,0 +1,41 @@
+package com.yunqing.demoleetcode.time;
+
+/**
+ * 12.20 贪心 + 栈
+ * @author yx
+ * @since 2020/12/21 10:18
+ */
+public class Time12m20 {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Solution12m20 {
+    public String removeDuplicateLetters(String s) {
+        boolean[] vis = new boolean[26];
+        int[] num = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            num[s.charAt(i) - 'a']++;
+        }
+
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (!vis[ch - 'a']) {
+                while (sb.length() > 0 && sb.charAt(sb.length() - 1) > ch) {
+                    if (num[sb.charAt(sb.length() - 1) - 'a'] > 0) {
+                        vis[sb.charAt(sb.length() - 1) - 'a'] = false;
+                        sb.deleteCharAt(sb.length() - 1);
+                    } else {
+                        break;
+                    }
+                }
+                vis[ch - 'a'] = true;
+                sb.append(ch);
+            }
+            num[ch - 'a'] -= 1;
+        }
+        return sb.toString();
+    }
+}
