7ffaec48732a2d1c8298bbedda72300d0d15f29c
zl_yunqing163 <yunqing_71@163.com>
2020-12-23 09:45:42 +0800

    12.23 hash表

---
+++ demo-leetcode/src/main/java/com/yunqing/demoleetcode/time/Time12m23.java
@@ -0,0 +1,31 @@
+package com.yunqing.demoleetcode.time;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 12.23 hash表
+ * @author yx
+ * @since 2020/12/23 9:41
+ */
+public class Time12m23 {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Solution12m23 {
+    public int firstUniqChar(String s) {
+        final char[] chars = s.toCharArray();
+        Map<Character, Integer> map = new HashMap<>();
+        for (char c : chars) {
+            final Integer orDefault = map.getOrDefault(c, 0);
+            map.put(c, orDefault + 1);
+        }
+        for (int i = 0; i < chars.length; i++) {
+            final Integer integer = map.get(chars[i]);
+            if (integer == 1) return i;
+        }
+        return -1;
+    }
+}
