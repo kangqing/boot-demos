a0b723af9adccc067eb5cd3fc9caf63acd0efc15
zl_yunqing163 <yunqing_71@163.com>
2020-12-24 11:15:32 +0800

    剑指Offer 03 数组中重复的数字

---
+++ demo-leetcode/src/main/java/com/yunqing/demoleetcode/offer/Offer03.java
@@ -0,0 +1,24 @@
+package com.yunqing.demoleetcode.offer;
+
+import java.util.Arrays;
+
+/**
+ * 剑指Offer 03 数组中重复的数字
+ * @author yx
+ * @since 2020/12/24 11:13
+ */
+public class Offer03 {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Solution {
+    public int findRepeatNumber(int[] nums) {
+        Arrays.sort(nums);
+        for(int i = 1; i < nums.length; i++) {
+            if(nums[i] == nums[i - 1]) return nums[i];
+        }
+        return -1;
+    }
+}
