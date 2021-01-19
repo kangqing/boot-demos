eb5ed7e018a4c4f9e8f8c4f5de821d382c707ad7
zl_yunqing163 <yunqing_71@163.com>
2021-01-19 17:35:27 +0800

    👽 复习多线程创建 3 种方式

---
+++ demo-a-test/src/main/java/com/yunqing/demoatest/multithread/thread/DemoCallable.java
@@ -0,0 +1,56 @@
+package com.yunqing.demoatest.multithread.thread;
+
+import lombok.SneakyThrows;
+
+import java.util.concurrent.*;
+
+/**
+ * 多线程创建的第三种方法，实现 Callable接口
+ * @author kangqing
+ * @since 2021/1/19 17:21
+ */
+public class DemoCallable {
+
+    @SneakyThrows
+    public static void main(String[] args) {
+        /**
+         * 核心线程数2
+         * 最大线程数10
+         * 线程池中超过corePoolSize数目的空闲线程最大存活时间 30秒
+         * 阻塞队列----new ArrayBlockingQueue
+         */
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(5, 100, 30L,
+                TimeUnit.SECONDS, new ArrayBlockingQueue<>(10));
+
+        final Future<Object> future = executor.submit(() -> "通过实现Callable接口,创建线程");
+        String res = (String) future.get();
+        System.out.println(res);
+
+
+        // 复习创建方法 1：继承Thread
+        method1();
+        // 复习方法 2： 实现Runnable
+        method2();
+    }
+
+    private static void method2() {
+        final Thread thread = new Thread(() -> {
+            System.out.println("创建多线程方法2： 实现Runnable接口");
+        });
+        thread.start();
+    }
+
+    private static void method1() {
+        final Thread thread = new Thread() {
+            @Override
+            public void run() {
+                System.out.println("创建多线程方法 1：继承Thread");
+                super.run();
+            }
+        };
+        thread.start();
+
+    }
+
+
+}
