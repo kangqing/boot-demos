dcdff715972dd8656742c6027bf4cf1001e036ec
zl_yunqing163 <yunqing_71@163.com>
2021-01-21 16:51:56 +0800

    🐞 中断线程

--- demo-a-test/src/main/java/com/yunqing/demoatest/multithread/thread/interrupt/InterruptDemo.java
+++ demo-a-test/src/main/java/com/yunqing/demoatest/multithread/thread/interrupt/InterruptDemo.java
@@ -5,38 +5,51 @@ import lombok.SneakyThrows;
 import java.util.concurrent.TimeUnit;
 
 /**
+ * 最后总结，关于这三个方法，interrupt（）是给线程设置中断标志；interrupted（）是检测中断并清除中断状态；isInterrupted（）只检测中断。
+ * 还有重要的一点就是interrupted（）作用于当前线程，interrupt（）和isInterrupted（）作用于此线程，即代码中调用此方法的实例所代表的线程。
  * interrupt 中断信号
  * @author kangqing
  * @since 2021/1/20 21:57
  */
 public class InterruptDemo {
+    @SneakyThrows
     public static void main(String[] args) {
-        // 线程 1 睡眠 3 秒
-        Thread thread1 = new Thread() {
-            @SneakyThrows
-            @Override
-            public void run() {
-                TimeUnit.SECONDS.sleep(3);
-                super.run();
-            }
-        };
-        // 线程 2 死循环
-        Thread thread2 = new Thread(() -> {
-            // 死循环
-            while (true) {
-                //System.out.println("死循环---");
+
+        for (int i = 0; i < 3; i++) {
+            TimeUnit.SECONDS.sleep(1);
+            System.out.println("主线程" + Thread.currentThread().getName() + "输出 = " + i);
+        }
+
+        Thread thread = new Thread(() -> {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    TimeUnit.SECONDS.sleep(1);
+                    System.out.println("输出 = " + i);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                    System.out.println("-----------------------------------------------------输出 = " + i);
+                }
             }
         });
 
-        thread1.start();
-        thread2.start();
-        // 添加中断信号
-        thread1.interrupt();
-        thread2.interrupt();
+        thread.start();
 
-        System.out.println("线程1中断状态 = " + thread1.isInterrupted());
-        System.out.println("线程2中断状态 = " + thread2.isInterrupted());
+        //TimeUnit.SECONDS.sleep(3);
+        System.out.println("仅输出中断状态 = " + thread.isInterrupted());
+        System.out.println("仅输出中断状态 = " + thread.isInterrupted() + "两次输出证明没有改变中断状态");
+        // 中断，相当于添加中断标志位，不影响程序继续输出
+        thread.interrupt();
+        Thread.currentThread().interrupt();
 
-        System.out.println("线程1中断状态 = " + thread1.isInterrupted());
+        // 主线程输出中断状态并删除中断状态, interrupted()操作的是当前线程
+        final boolean interrupted = Thread.interrupted();
+        System.out.println("------------------" + interrupted);
+        System.out.println("------------------" + Thread.interrupted());
+
+        System.out.println("最后再次打印仅输出中断状态" + thread.isInterrupted());
+        System.out.println("主线程中断状态" + Thread.currentThread().isInterrupted());
+
+
     }
+
 }
