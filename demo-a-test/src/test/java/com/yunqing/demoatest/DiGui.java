19c57d5ba104f8e362cb97d3132551254c7bb807
zl_yunqing163 <yunqing_71@163.com>
2021-02-04 17:39:44 +0800

    ☔ 阶乘

--- demo-a-test/src/test/java/com/yunqing/demoatest/DiGui.java
+++ demo-a-test/src/test/java/com/yunqing/demoatest/DiGui.java
@@ -27,6 +27,17 @@ public class DiGui {
         int i = 10;
         System.out.println(diGui.fibonacci(i));
 
+        System.out.println(dp(15));
+
+    }
+
+    private static int dp(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        for (int i = 1; i <= n ; i++) {
+            dp[i] = dp[i - 1] * i;
+        }
+        return dp[n];
     }
 }
 
