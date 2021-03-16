f40a007487d84ed2f468666b3e54430b7b241f28
kangqing <kangqing.37@gmail.com>
2021-03-16 09:50:12 +0800

    🧙 冒泡排序优化，上浮 + 下沉 + 未移动结束判断

--- demo-leetcode/src/main/java/com/yunqing/demoleetcode/sort/BubbleSort.java
+++ demo-leetcode/src/main/java/com/yunqing/demoleetcode/sort/BubbleSort.java
@@ -19,6 +19,7 @@ public class BubbleSort {
 
         // 第 i 趟排序
         for (int i = 0; i < arr.length - 1; i++) {
+            boolean b = false;
             // 此处多减了个 i 是因为后面的大数已经确定 i 个了，就不用比较了
             for (int j = 0; j < arr.length - 1 - i; j++) {
                 // 相邻两个数比对交换，小的在前，大的在后
@@ -26,11 +27,28 @@ public class BubbleSort {
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
+                    b = true;
+                } else {
+                    b = false;//如果都没有交换的，说明已经有序
                 }
                 System.out.println("第" + j + "和" + (j + 1) + "索引比较交换后 ：" + Arrays.toString(arr));
             }
+            if (!b) break;
+            // 同时从后面开始，从大到小排序
+            for (int j = arr.length - 1 - i; j > 0 ; j--) {
+                if (arr[j] < arr[j - 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = temp;
+                    b = true;
+                } else {
+                    b = false;
+                }
+                System.out.println("逆向 ： " + Arrays.toString(arr));
+            }
             // 第 i 趟排序后的结果
             System.out.println("----------------------第" + i + "趟排序后的结果：" + Arrays.toString(arr));
+            if (!b) break;
         }
     }
 }
