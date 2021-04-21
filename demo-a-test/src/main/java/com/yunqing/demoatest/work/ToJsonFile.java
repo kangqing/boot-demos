43c638755b77303800d848b059ec272f352da74b
kangqing <kangqing.37@gmail.com>
2021-04-21 19:24:10 +0800

    拼装 json 文件，压缩成 zip 包

---
+++ demo-a-test/src/main/java/com/yunqing/demoatest/work/ToJsonFile.java
@@ -0,0 +1,73 @@
+package com.yunqing.demoatest.work;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONException;
+import com.alibaba.fastjson.JSONObject;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.*;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.zip.ZipOutputStream;
+
+/**
+ * 封装一个 json 文件
+ * @author kangqing
+ * @since 2021/4/20 19:14
+ */
+public class ToJsonFile {
+    public static void main(String[] args) throws Exception {
+        List<Record> list = List.of(new Record("1", "记录1", "110"),
+                new Record("2", "记录2", "119"),
+                new Record("3", "记录3", "120"));
+        List<Record> collect = list.stream().limit(2).collect(Collectors.toList());
+        createJsonFile(list);
+
+    }
+
+    private static void createJsonFile(List<Record> list) throws Exception {
+        File file = new File("searchData.json");
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("id", "1111");
+        jsonObject.put("checkCode", "checkCode");
+
+
+
+        // java 对象列表转成 json 数组，先转成 json 字符串，再转成 jsonArray
+        JSONArray objects = JSONArray.parseArray(JSON.toJSONString(list));
+        jsonObject.put("data", objects);
+
+        try {
+
+            FileWriter fileWriter = new FileWriter(file);
+            System.out.println("Writing JSON object to file");
+            System.out.println("-----------------------");
+            System.out.print(jsonObject);
+            // 向文件中写入内容
+            fileWriter.write(jsonObject.toString());
+            fileWriter.flush();
+            fileWriter.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // 给压缩包命名 hello.zip 并且创建到指定目录
+        File zipFile = new File("/Users/yunqing/Documents" + File.separator + "hello.zip");
+        ZipOutputStream zipOut = new ZipOutputStream(new FileOutputStream(zipFile));
+        //ZipUtils.compress(file, zipOut, "searchData.json", true);
+        ZipUtils.toZip(file, zipOut, true);
+        //ZipUtils.toZip(List.of(file), zipOut);
+    }
+}
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+class Record {
+    private String id;
+    private String name;
+    private String mobile;
+}
