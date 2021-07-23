f58c654d4e6e4ea9ee423a0209d15a5e88001052
kangqing <kangqing.37@gmail.com>
2021-07-23 15:41:02 +0800

    获取浏览器版本号

--- demo-a-test/src/main/java/com/yunqing/demoatest/controller/HttpClientController.java
+++ demo-a-test/src/main/java/com/yunqing/demoatest/controller/HttpClientController.java
@@ -2,7 +2,12 @@ package com.yunqing.demoatest.controller;
 
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * 测试http client
@@ -27,5 +32,73 @@ public class HttpClientController {
     @GetMapping("/testNow")
     public String getStrNow() {
         return "立刻返回结果";
+    }
+
+    @GetMapping("/version")
+    public Map<String,String> getUserAgent(HttpServletRequest req){
+        Map<String,String> Sys= new HashMap<String, String>();
+        String ua = req.getHeader("User-Agent").toLowerCase();
+        System.out.println("ua====" + ua);
+        String s;
+        String msieP = "msie ([\\d.]+)";
+        String ie11 = "rv:([\\d.]+)";
+        String firefoxP = "firefox\\/([\\d.]+)";
+        String chromeP = "chrome\\/([\\d.]+)";
+        String operaP = "opera.([\\d.]+)";
+        String safariP = "version\\/([\\d.]+).*safari";
+
+        Pattern pattern = Pattern.compile(msieP);
+        Matcher mat = pattern.matcher(ua);
+        if(mat.find()){
+            s=mat.group();
+            Sys.put("type", "ie");
+            Sys.put("version", s.split(" ")[1]);
+            return Sys;
+        }
+
+        pattern = Pattern.compile(ie11);
+        mat = pattern.matcher(ua);
+        if (mat.find()) {
+            s=mat.group();
+            Sys.put("type", "ie11");
+            Sys.put("version", s.split(":")[1]);
+            return Sys;
+        }
+
+        pattern = Pattern.compile(firefoxP);
+        mat=pattern.matcher(ua);
+        if(mat.find()){
+            s=mat.group();
+            System.out.println(s);
+            Sys.put("type", "firefox");
+            Sys.put("version", s.split("/")[1]);
+            return Sys;
+        }
+        pattern = Pattern.compile(chromeP);
+        mat=pattern.matcher(ua);
+        if(mat.find()){
+            s=mat.group();
+            Sys.put("type", "chrome");
+            Sys.put("version", s.split("/")[1]);
+            return Sys;
+        }
+        pattern = Pattern.compile(operaP);
+        mat=pattern.matcher(ua);
+        if(mat.find()){
+            s=mat.group();
+            System.out.println(s);
+            Sys.put("type", "opera");
+            Sys.put("version", s.split("\\.")[1]);
+            return Sys;
+        }
+        pattern = Pattern.compile(safariP);
+        mat=pattern.matcher(ua);
+        if(mat.find()){
+            s=mat.group();
+            Sys.put("type", "safari");
+            Sys.put("version", s.split("/")[1].split(" ")[0]);
+            return Sys;
+        }
+        return Sys;
     }
 }
