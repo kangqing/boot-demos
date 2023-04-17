85644dbef396434d15e2edda093b7f7c5195a6ce
kangqing <kangqing.37@gmail.com>
2023-04-17 21:20:41 +0800

    享元模式

---
+++ design-pattern/src/main/java/flyweight/ChessPiece.java
@@ -0,0 +1,20 @@
+package flyweight;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * 棋子类
+ * @author kangqing
+ * @since 2023/4/17 21:09
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ChessPiece {
+     // 棋子使用享元类对象
+     private ChessPieceUnit chessPieceUnit;
+     private int positionX;
+     private int positionY;
+}
