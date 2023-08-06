9395756dbf4940ac668e155784ef5f426b801521
kangqing <kangqing.37@gmail.com>
2023-08-06 21:15:28 +0800

    oss对象存储测试

--- demo-oss/src/main/java/com/yunqing/DemoOssApplication.java
+++ demo-oss/src/main/java/com/yunqing/DemoOssApplication.java
@@ -1,5 +1,16 @@
-package com.yunqing;/**
+package com.yunqing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
  * @author kangqing
  * @since 2023/8/4 20:09
- */public class DemoOssApplication {
+ */
+@SpringBootApplication
+public class DemoOssApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoOssApplication.class, args);
+    }
 }
--- demo-oss/src/main/java/com/yunqing/TestController.java
+++ demo-oss/src/main/java/com/yunqing/TestController.java
@@ -1,5 +1,51 @@
-package com.yunqing;/**
+package com.yunqing;
+
+import com.yunqing.api.OssOperateRpcApi;
+import com.yunqing.dto.OssProcessDTO;
+import com.yunqing.dto.RpcRequest;
+import com.yunqing.dto.RpcResponse;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.annotation.Resource;
+
+/**
  * @author kangqing
  * @since 2023/8/4 20:10
- */public class TestController {
+ */
+@RestController
+public class TestController {
+
+    @Resource
+    private OssOperateRpcApi operateRpcApi;
+
+    @GetMapping("/create")
+    public RpcResponse<?> createBucket(String name) {
+        RpcRequest<String> rpcRequest = new RpcRequest<>(name);
+        try {
+            operateRpcApi.create(rpcRequest);
+            return RpcResponse.success();
+        } catch (Exception e) {
+            throw new RuntimeException("创建 Bucket 失败！");
+        }
+    }
+
+    @PostMapping("/upload")
+    public RpcResponse<?> uploadFile(MultipartFile file) {
+
+        OssProcessDTO dto = OssProcessDTO.builder()
+                .file(file)
+                .fileName(file.getOriginalFilename())
+                .build();
+        final RpcRequest<OssProcessDTO> request = new RpcRequest<>(dto);
+
+        return operateRpcApi.upload(request);
+
+    }
+
+
+
+
 }
