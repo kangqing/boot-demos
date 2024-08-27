ece898f98c5fe9b20e81459fe2531243f7402559
11452 <kangqing.37@gmail.com>
2024-08-27 17:12:16 +0800

    feature:如何发送告警信息到integration中

--- demo-integration/src/main/java/com/yunqing/APITest.java
+++ demo-integration/src/main/java/com/yunqing/APITest.java
@@ -1,16 +1,26 @@
 package com.yunqing;
 
+import org.springframework.integration.support.MessageBuilder;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.time.Instant;
+
 /**
  * @author kangqing
  * @since 2024/8/26 16:14
  */
+@Service
 public class APITest {
 
-    public static void main(String[] args) {
+    @Resource
+    private MessageChannel alarmEventChannel;
 
+    public void test() {
         // Send to alarmEventChannel
-//        alarmEventChannel.send(MessageBuilder.withPayload(alarmInfo)
-//                .setHeader("time", Instant.now())
-//                .build());
+        alarmEventChannel.send(MessageBuilder.withPayload(new AlarmInfo())
+                .setHeader("time", Instant.now())
+                .build());
     }
 }
